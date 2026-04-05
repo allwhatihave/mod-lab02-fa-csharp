1d217b46fc7f124d2201fd383cac95227e15a190
allwhatihave <kirillit2016tig@gmail.com>
2026-04-05 21:47:07 +0300

    Refactor finite automata classes and improve transitions

--- fa/program.cs
+++ fa/program.cs
@@ -16,31 +16,37 @@ namespace fans
         }
     }
 
+    // FA1: ровно один '0' и хотя бы одна '1'
     public class FA1
     {
-        State s0, s1, s2, dead;
+        State s0, s1, s01, s11, dead;
         State InitialState;
 
         public FA1()
         {
-            s0 = new State("s0");
-            s1 = new State("s1");
-            s2 = new State("s2") { IsAcceptState = true };
-            dead = new State("dead");
+            s0 = new State("s0");       // старт
+            s1 = new State("s1");       // видели хотя бы одну '1', ноль ещё нет
+            s01 = new State("s01");     // видели ровно один '0', пока нет '1'
+            s11 = new State("s11") { IsAcceptState = true }; // ровно один '0' и хотя бы одна '1'
+            dead = new State("dead");   // все остальные случаи
 
             InitialState = s0;
 
             // s0
-            s0.Transitions['0'] = s2;
+            s0.Transitions['0'] = s01;
             s0.Transitions['1'] = s1;
 
             // s1
-            s1.Transitions['0'] = s2;
+            s1.Transitions['0'] = s11;
             s1.Transitions['1'] = s1;
 
-            // s2
-            s2.Transitions['0'] = dead;
-            s2.Transitions['1'] = s2;
+            // s01
+            s01.Transitions['0'] = dead;
+            s01.Transitions['1'] = s11;
+
+            // s11
+            s11.Transitions['0'] = dead;
+            s11.Transitions['1'] = s11;
 
             // dead
             dead.Transitions['0'] = dead;
@@ -60,17 +66,18 @@ namespace fans
         }
     }
 
+    // FA2: нечётное количество '0' и нечётное количество '1'
     public class FA2
     {
-        State ee, oe, eo, oo;
+        State ee, eo, oe, oo;
         State InitialState;
 
         public FA2()
         {
-            ee = new State("ee");
-            oe = new State("oe");
-            eo = new State("eo");
-            oo = new State("oo") { IsAcceptState = true };
+            ee = new State("ee"); // чётное 0, чётное 1
+            eo = new State("eo"); // чётное 0, нечётное 1
+            oe = new State("oe"); // нечётное 0, чётное 1
+            oo = new State("oo") { IsAcceptState = true }; // нечётное 0, нечётное 1
 
             InitialState = ee;
 
@@ -100,6 +107,7 @@ namespace fans
         }
     }
 
+    // FA3: содержит '11'
     public class FA3
     {
         State s0, s1, s2;
@@ -143,13 +151,13 @@ namespace fans
             string s = "01111";
 
             FA1 fa1 = new FA1();
-            Console.WriteLine(fa1.Run(s));
+            Console.WriteLine($"FA1('{s}') = {fa1.Run(s)}");
 
             FA2 fa2 = new FA2();
-            Console.WriteLine(fa2.Run(s));
+            Console.WriteLine($"FA2('{s}') = {fa2.Run(s)}");
 
             FA3 fa3 = new FA3();
-            Console.WriteLine(fa3.Run(s));
+            Console.WriteLine($"FA3('{s}') = {fa3.Run(s)}");
         }
     }
 }

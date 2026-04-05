822449e3a73d8b9075a60325679754e5da231b86
allwhatihave <kirillit2016tig@gmail.com>
2026-04-05 21:40:15 +0300

    Refactor unit tests for FA1, FA2, and FA3

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -1,107 +1,188 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using fans;
-
 namespace NET
 {
     [TestClass]
     public class UnitTest1
     {
-        // ===== FA1 =====
-
         [TestMethod]
-        public void FA1_Test1()
+        public void TestMethod1()
         {
-            var fa = new FA1();
-            Assert.IsTrue(fa.Run("10") == true);
+            String s = "0111";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            String s = "01011";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }   
+        [TestMethod]
+        public void TestMethod3()
+        {
+            String s = "110101011";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }  
+        [TestMethod]
+        public void TestMethod4()
+        {
+            String s = "1110111";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }  
+        [TestMethod]
+        public void TestMethod5()
+        {
+            String s = "10";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        } 
+        [TestMethod]
+        public void TestMethod6()
+        {
+            String s = "0101";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }  
+        [TestMethod]
+        public void TestMethod7()
+        {
+            String s = "00110011";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }  
+        [TestMethod]
+        public void TestMethod8()
+        {
+            String s = "0001";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }  
+        [TestMethod]
+        public void TestMethod9()
+        {
+            String s = "111000";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        } 
+        [TestMethod]
+        public void TestMethod10()
+        {
+            String s = "00110011";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            String s = "0101";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }     
+        }
+         [TestMethod]
+        public void TestMethod12()
+        {
+            String s = "0";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA1_Test2()
+        public void TestMethod13()
         {
-            var fa = new FA1();
-            Assert.IsTrue(fa.Run("101") == true);
+            String s = "1";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA1_Test3()
+        public void TestMethod14()
         {
-            var fa = new FA1();
-            Assert.IsTrue(fa.Run("1110") == true);
+            String s = "";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA1_Test4()
+        public void TestMethod15()
         {
-            var fa = new FA1();
-            Assert.IsTrue(fa.Run("0") == false);
+            String s = "01";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
         }
 
         [TestMethod]
-        public void FA1_Test5()
+        public void TestMethod16()
         {
-            var fa = new FA1();
-            Assert.IsTrue(fa.Run("1001") == false);
-        }
-
-        // ===== FA2 =====
-
-        [TestMethod]
-        public void FA2_Test1()
-        {
-            var fa = new FA2();
-            Assert.IsTrue(fa.Run("01") == true);
+            String s = "101001";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
         }
 
         [TestMethod]
-        public void FA2_Test2()
+        public void TestMethod17()
         {
-            var fa = new FA2();
-            Assert.IsTrue(fa.Run("011") == true);
+            String s = "0011";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA2_Test3()
+        public void TestMethod18()
         {
-            var fa = new FA2();
-            Assert.IsTrue(fa.Run("001") == false);
+            String s = "1111";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA2_Test4()
+        public void TestMethod19()
         {
-            var fa = new FA2();
-            Assert.IsTrue(fa.Run("0011") == false);
+            String s = "11";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
         }
 
         [TestMethod]
-        public void FA2_Test5()
+        public void TestMethod20()
         {
-            var fa = new FA2();
-            Assert.IsTrue(fa.Run("111000") == true);
-        }
-
-        // ===== FA3 =====
-
-        [TestMethod]
-        public void FA3_Test1()
-        {
-            var fa = new FA3();
-            Assert.IsTrue(fa.Run("11") == true);
+            String s = "";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
 
         [TestMethod]
-        public void FA3_Test2()
+        public void TestMethod21()
         {
-            var fa = new FA3();
-            Assert.IsTrue(fa.Run("1011") == true);
-        }
-
-        [TestMethod]
-        public void FA3_Test3()
-        {
-            var fa = new FA3();
-            Assert.IsTrue(fa.Run("0101") == false);
+            String s = "0000";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
         }
     }
 }
